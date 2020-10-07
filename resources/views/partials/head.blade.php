5febfa373ece3b826e1daf19b14998b025491e1b
Rydzkowski Grzegorz <grzegorz.rydzkowski@gdansk.gda.pl>
2020-10-07 13:07:44 +0200

    add site scale

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,7 +1,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="viewport" content="width=device-width, initial-scale=0.9, shrink-to-fit=no">
   @php wp_head() @endphp
   <!-- Global site tag (gtag.js) - Google Analytics -->
 {{-- <script async src=“https://www.googletagmanager.com/gtag/js?id=UA-178419658-1”></script>
